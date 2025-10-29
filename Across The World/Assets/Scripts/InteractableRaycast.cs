6bc46ecf0d1ccf8aa16ea36e0d18a41ebb873bec
Joseph <jpraposo2027@gmail.com>
2025-10-29 01:44:07 -0400

    added interactable script

---
+++ Across The World/Assets/Scripts/InteractableRaycast.cs
@@ -0,0 +1,56 @@
+using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractableRaycast : MonoBehaviour
+{
+    private float raylength = 5;
+
+    private KeyCode killEnemy = KeyCode.Mouse0;
+
+    public Image crosshair;
+
+    private const string interactableTag = "Enemy";
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        //Button Raycast
+        if (Physics.Raycast(transform.position, forward, out hit, raylength))
+        {
+            if (hit.collider.CompareTag(interactableTag))
+            {
+                CrosshairChange(true);
+                if (Input.GetKeyDown(killEnemy))
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+        else
+        {
+            CrosshairChange(false);
+        }
+    }
+
+    void CrosshairChange(bool on)
+    {
+        if (on)
+        {
+            crosshair.color = Color.red;
+        }
+        else
+        {
+            crosshair.color = Color.white;
+        }
+    }
+}
